72c221b6ce2b9b692c2f95bfb0c7ef0da624f034
Илья Быков <BIU@localhost.localdomain>
2020-11-04 21:58:26 +0200

    Add page Genres and counter sessions

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,11 +6,11 @@ from django.conf.urls import url
 urlpatterns = [
     path('', views.index, name='index'),
  	path('books/', views.BookListView.as_view(), name='books'),
- 	path('books/', views.BookListView.as_view(), name='books'),
  	path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('author/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>',
          views.AuthorDetailView.as_view(), name='author-detail'),
+         path('genres/', views.GenreListView.as_view(), name='genres')
  	#path('catalog/books/', ),
  	#path('catalog/authors/', ),
  	#path('catalog/book/<id>', ),
--- catalog/views.py
+++ catalog/views.py
@@ -10,16 +10,20 @@ def index(request):
     # Генерация "количеств" некоторых главных объектов
     num_books=Book.objects.all().count()
     num_instances=BookInstance.objects.all().count()
+    num_genres = Genre.objects.all().count()
     # Доступные книги (статус = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # Метод 'all()' применен по умолчанию.
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     
     # Отрисовка HTML-шаблона index.html с данными внутри 
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_visits':num_visits, 'num_genres':num_genres}
     )
     
     
@@ -35,4 +39,6 @@ class AuthorListView(generic.ListView):
 class AuthorDetailView(generic.DetailView):
     model = Author
     
+class GenreListView(generic.ListView):
+    model = Genre
 
